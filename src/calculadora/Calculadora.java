9e413d79397720a22b0eeb9dbac2ac85f4d978e0
alexm <alexm@DESKTOP-MJ0D4HD>
2018-09-26 17:06:26 +0200

    Control de entrada de datos

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,12 +3,38 @@ package calculadora;
 import java.util.Scanner;
 
 public class Calculadora {
+    static boolean bueno=false;
  
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Introduce dos numeros");
-        int n1=sc.nextInt();
-        int n2=sc.nextInt();
+        int n1=0;
+        int n2=0;
+            
+         do {    
+               try {
+            System.out.println("Introduce el primer numero");
+            n1=sc.nextInt(); 
+            bueno=true;
+        } catch (Exception e) {
+            System.out.println("Debes introducir un numero");
+            bueno=false;
+            sc.next();
+        }
+        } while (!bueno);
+         
+        do { 
+            try {
+                System.out.println("Introduce el segundo numero (no puede ser 0)");
+                n2=sc.nextInt();
+                bueno=true;
+            } catch (Exception e) {
+                System.out.println("Debes introducir un numero y no puede ser 0");
+                bueno=false;
+                sc.next();
+            }
+            
+        } while (!bueno||n2==0);
+        
         System.out.println("La suma de tus dos numeros "+n1+" + "+n2+" es: "+suma(n1, n2));
         System.out.println("La resta de tus dos numeros "+n1+" - "+n2+" es: "+resta(n1, n2));
         System.out.println("La multiplicacion de tus dos numeros "+n1+" * "+n2+" es: "+multiplicacio(n1, n2));
