4f43187d768598b72550089190c858d8386463a5
alexm <alexm@DESKTOP-MJ0D4HD>
2018-09-26 16:19:05 +0200

    Pedimos dos números al usuario

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,10 +1,19 @@
 
 package calculadora;
+import java.util.Scanner;
 
 public class Calculadora {
-
+ 
+    static int numero1=0;
+    static int numero2=0;
   
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduce dos numeros");
+        numero1=sc.nextInt();
+        numero2=sc.nextInt();
+        
+        
    
     }
     
