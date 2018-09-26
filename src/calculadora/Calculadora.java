3f4d654ceb01797f41692f0047703486a08317c3
alexm <alexm@DESKTOP-MJ0D4HD>
2018-09-26 16:41:37 +0200

    Añadimos funcion multiplicacion, cambiamos el nombre de los numeros y borramos dos variables que no se utilizaban.

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Calculadora {
  
     public static void main(String[] args) {
-        int a,b;
         Scanner sc=new Scanner(System.in);
         System.out.println("Introduce dos numeros");
-        int numero1=sc.nextInt();
-        int numero2=sc.nextInt();
-        System.out.println("La suma de tus dos numero "+numero1+" + "+numero2+" es: "+suma(numero1, numero2));
-        System.out.println("La resta de tus dos numero "+numero1+" - "+numero2+" es: "+resta(numero1, numero2));
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.println("La suma de tus dos numeros "+n1+" + "+n2+" es: "+suma(n1, n2));
+        System.out.println("La resta de tus dos numeros "+n1+" - "+n2+" es: "+resta(n1, n2));
+        System.out.println("La multiplicacion de tus dos numeros "+n1+" * "+n2+" es: "+multiplicacio(n1, n2));
     }
     
     public  static int suma(int numero1,int numero2){
@@ -20,6 +20,10 @@ public class Calculadora {
     
     public  static int resta(int numero1,int numero2){
         return numero1-numero2;
+    }
+    
+    public  static int multiplicacio(int numero1,int numero2){
+        return numero1*numero2;
     }
     
     
