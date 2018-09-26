99b0a8ec68123e47f2bddb45ad12a79f82d3caf0
alexm <alexm@DESKTOP-MJ0D4HD>
2018-09-26 16:32:14 +0200

    Añadimos función suma y dos varialbles irrelevantes

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Calculadora {
  
-    static int numero1=0;
-    static int numero2=0;
-  
     public static void main(String[] args) {
+        int a,b;
         Scanner sc=new Scanner(System.in);
         System.out.println("Introduce dos numeros");
-        numero1=sc.nextInt();
-        numero2=sc.nextInt();
-        
-        
-   
+        int numero1=sc.nextInt();
+        int numero2=sc.nextInt();
+        System.out.println("La suma de tus dos numero "+numero1+"+"+numero2+" es: "+suma(numero1, numero2));
+    }
+    
+    public  static int suma(int numero1,int numero2){
+        return numero1+numero2;
     }
     
 }
