18812f360a8591b5a34a7df7c1aaecc7431c9e7c
alexm <alexm@DESKTOP-MJ0D4HD>
2018-09-26 16:43:40 +0200

    Añadimos la funcion division

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -12,6 +12,7 @@ public class Calculadora {
         System.out.println("La suma de tus dos numeros "+n1+" + "+n2+" es: "+suma(n1, n2));
         System.out.println("La resta de tus dos numeros "+n1+" - "+n2+" es: "+resta(n1, n2));
         System.out.println("La multiplicacion de tus dos numeros "+n1+" * "+n2+" es: "+multiplicacio(n1, n2));
+        System.out.println("La division de tus dos numeros "+n1+" / "+n2+" es: "+divisio(n1, n2));
     }
     
     public  static int suma(int numero1,int numero2){
@@ -25,6 +26,11 @@ public class Calculadora {
     public  static int multiplicacio(int numero1,int numero2){
         return numero1*numero2;
     }
+    
+    public  static int divisio(int numero1,int numero2){
+        return numero1/numero2;
+    }
+    
     
     
     
