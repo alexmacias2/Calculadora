55c28c0508f3efa22d059ae5da04ea5869ee5ad4
alexm <alexm@DESKTOP-MJ0D4HD>
2018-09-26 16:36:38 +0200

    Añadimos función resta

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -10,11 +10,18 @@ public class Calculadora {
         System.out.println("Introduce dos numeros");
         int numero1=sc.nextInt();
         int numero2=sc.nextInt();
-        System.out.println("La suma de tus dos numero "+numero1+"+"+numero2+" es: "+suma(numero1, numero2));
+        System.out.println("La suma de tus dos numero "+numero1+" + "+numero2+" es: "+suma(numero1, numero2));
+        System.out.println("La resta de tus dos numero "+numero1+" - "+numero2+" es: "+resta(numero1, numero2));
     }
     
     public  static int suma(int numero1,int numero2){
         return numero1+numero2;
     }
+    
+    public  static int resta(int numero1,int numero2){
+        return numero1-numero2;
+    }
+    
+    
     
 }
